979eb7dd900aff2470514e74c98f4d42afd473d7
Matthias Fuchs <matthias.s.fuchs@gmail.com>
2016-12-18 23:39:06 +0100

    init

---
+++ src/test/java/SlowFlakyTest123.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.ThreadLocalRandom;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SlowFlakyTest123 {
+    @Test
+    public void testFlaky1() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 2);
+    }
+
+    @Test
+    public void testFlaky2() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 1);
+    }
+
+    @Test
+    public void testFlaky3() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 1);
+    }
+
+    @Test
+    public void testFlaky4() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 1);
+    }
+
+    @Test
+    public void testFlaky5() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 1);
+    }
+
+    @Test
+    public void testFlaky6() {
+        int randNum = ThreadLocalRandom.current().nextInt(1, 11);
+        assertTrue(randNum > 1);
+    }
+}
